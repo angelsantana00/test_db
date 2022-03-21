82388073ef4dd5fcf06fd189a733e00294950293
Angel Santana <angel.d.santana00@gmail.com>
2022-03-21 09:46:43 -0500

    auto save

--- where_exercises.sql
+++ where_exercises.sql
@@ -32,16 +32,10 @@ FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
 
-
-
-
-
-
-doop
-
-
 SELECT last_name
 FROM employees
 WHERE last_name LIKE '%q%' AND last_name not  like  '%qu%';
+
+
 
 
